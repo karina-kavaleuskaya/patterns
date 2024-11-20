f5b27e9cbda2657f5cb35cf7b2354efce932219c
karina-kavaleuskaya <k.kavaleuskaya@mail.ru>
2024-11-20 17:56:28 +0300

    add different greeter classes

--- greeting.py
+++ greeting.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from enum import Enum
 
 
@@ -7,13 +8,52 @@ class GreetingType(Enum):
     NIGHT = 'Good night!'
 
 
-def greet(name: str, greeting: GreetingType):
-    greetings = {
-        GreetingType.MORNING: f"{GreetingType.MORNING.value} {name}",
-        GreetingType.DAY: f"{GreetingType.DAY.value} {name}",
-        GreetingType.NIGHT: f"{GreetingType.NIGHT.value} {name}"
+class Greeter(ABC):
+    def __init__(self, greeting: GreetingType):
+        self.greeting = greeting
+
+    @abstractmethod
+    def greet(self, name: str) -> str:
+        pass
+
+
+class MorningGreeter(Greeter):
+    def __init__(self):
+        super().__init__(GreetingType.MORNING)
+
+    def greet(self, name: str) -> str:
+        return f"{self.greeting.value} {name}"
+
+
+class DayGreeter(Greeter):
+    def __init__(self):
+        super().__init__(GreetingType.DAY)
+
+    def greet(self, name: str) -> str:
+        return f"{self.greeting.value} {name}"
+
+
+class NightGreeter(Greeter):
+    def __init__(self):
+        super().__init__(GreetingType.NIGHT)
+
+    def greet(self, name: str) -> str:
+        return f"{self.greeting.value} {name}"
+
+
+def greet(name: str, greeting: GreetingType) -> str:
+    greeting_classes = {
+        GreetingType.MORNING: MorningGreeter,
+        GreetingType.DAY: DayGreeter,
+        GreetingType.NIGHT: NightGreeter,
     }
-    return greetings.get(greeting)
+
+    greeter_class = greeting_classes.get(greeting)
+
+    greeter = greeter_class()
+    return greeter.greet(name)
+
+
 
 
 if __name__ == "__main__":
